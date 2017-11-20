51cee0eeed0a6c5bb021b7191d6ff30a97ba7187
Angel Manzano Alcaraz <amalcaraz89@gmail.com>
2017-11-20 15:22:31 +0100

    Added socket.io lib

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('hello!');
+import * as socketIO from 'socket.io';
+
+const io: SocketIO.Server = socketIO(3000);
+
+console.log('running server...');
+
+io.on('connection', function (socket) {
+
+  io.emit('this', { will: 'be received by everyone'});
+
+  socket.on('my other event', function (from, msg) {
+    console.log('I received a private message by ', from, ' saying ', msg);
+  });
+
+  socket.on('disconnect', function () {
+    io.emit('user disconnected');
+  });
+});
