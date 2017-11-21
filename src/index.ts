abf7e24bbcf745089db8051007940bb4edf9ab47
Angel Manzano Alcaraz <amalcaraz89@gmail.com>
2017-11-21 01:41:55 +0100

    Added WebRTC ICE and SDP message handlers

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,17 @@ console.log('running server...');
 
 io.on('connection', function (socket) {
 
-  io.emit('this', { will: 'be received by everyone'});
+    io.emit('this', {will: 'be received by everyone'});
 
-  socket.on('my other event', function (from, msg) {
-    console.log('I received a private message by ', from, ' saying ', msg);
-  });
+    socket.on('ice-candidate', function (from, msg) {
+        console.log('I received an ice candidate message by ', from, ' saying ', msg);
+    });
 
-  socket.on('disconnect', function () {
-    io.emit('user disconnected');
-  });
+    socket.on('sdp', function (from, msg) {
+        console.log('I received a sdp message by ', from, ' saying ', msg);
+    });
+
+    socket.on('disconnect', function () {
+        io.emit('user disconnected');
+    });
 });
