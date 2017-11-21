866dd57872dbdfa266bf6f102e48faaeecd08e64
Angel Manzano Alcaraz <amalcaraz89@gmail.com>
2017-11-21 19:05:53 +0100

    Partial progress in signaling implementation

---
+++ src/client-data.model.ts
@@ -0,0 +1,62 @@
+export interface ClientLoginData {
+    alias: string;
+}
+
+export class ClientData {
+    constructor(public socketId: string,
+                public alias: string,
+                public rooms: string[]) {
+    }
+}
+
+export class ClientDataStore {
+
+    private clientList: { [socketId: string]: ClientData };
+
+    getClientsDataListFromSocketMap(sockets: { [id: string]: SocketIO.Socket }): ClientData[] {
+        return Object
+            .keys(sockets)
+            .map((k: string) => sockets[k])
+            .reduce((prev: ClientData[], curr: SocketIO.Socket) => {
+                const clientData: ClientData | undefined = this.get(curr.id);
+                return clientData
+                    ? prev.concat(clientData)
+                    : prev;
+            }, []);
+    }
+
+    get(id: string): ClientData | undefined {
+
+        return this.clientList[id];
+
+    }
+
+    set(id: string, data: ClientData): void {
+
+        this.clientList[id] = data;
+
+    }
+
+    getByAlias(alias: string): ClientData | undefined {
+
+        return this.findByProperty('alias', alias);
+
+    }
+
+    setByAlias(alias: string, data: ClientData): void {
+
+        const i: string | undefined = this.findIndexByProperty('alias', alias);
+        if (i) this.clientList[i] = data;
+
+    }
+
+    private findIndexByProperty(prop: keyof ClientData, value: any): string | undefined {
+        return Object.keys(this.clientList).find((key: string) => this.clientList[key][prop] === value);
+    }
+
+    private findByProperty(prop: keyof ClientData, value: any): ClientData | undefined {
+        const i: string | undefined = this.findIndexByProperty(prop, value);
+        return i ? this.clientList[i] : undefined;
+    }
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,48 @@
 import * as socketIO from 'socket.io';
+import { ClientData, ClientDataStore, ClientLoginData } from './client-data.model';
 
 const io: SocketIO.Server = socketIO(3000);
+const signAlignSockets = io.of('/signaling');
 
 console.log('running server...');
 
-io.on('connection', function (socket) {
+const clientDataStore = new ClientDataStore();
 
-    io.emit('this', {will: 'be received by everyone'});
+signAlignSockets
+    .on('connection', (socket: SocketIO.Socket) => {
 
-    socket.on('ice-candidate', function (from, msg) {
-        console.log('I received an ice candidate message by ', from, ' saying ', msg);
+        socket.on('login', (loginData: ClientLoginData) => {
+
+            clientDataStore.set(
+                socket.id,
+                new ClientData(socket.id, loginData.alias, [])
+            );
+
+            signAlignSockets.emit('new-peer', clientDataStore.get(socket.id));
+            socket.emit('peer-list', clientDataStore.getClientsDataListFromSocketMap(signAlignSockets.sockets));
+
+        });
+
+        socket.on('ice-candidate', function (from, msg) {
+            console.log('I received an ice candidate message by ', from, ' saying ', msg);
+        });
+
+        socket.on('sdp', function (from, msg) {
+            console.log('I received a sdp message by ', from, ' saying ', msg);
+        });
+
+        socket.on('disconnect', function () {
+
+            socket.on('login', (loginData: ClientLoginData) => {
+
+                clientDataStore.set(
+                    socket.id,
+                    new ClientData(socket.id, loginData.alias, [])
+                );
+
+            });
+
+            signAlignSockets.emit('peer-disconnected');
+
+        });
     });
-
-    socket.on('sdp', function (from, msg) {
-        console.log('I received a sdp message by ', from, ' saying ', msg);
-    });
-
-    socket.on('disconnect', function () {
-        io.emit('user disconnected');
-    });
-});
